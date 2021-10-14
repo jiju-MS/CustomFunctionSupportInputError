c1c82c3dd738a31ddbc58166da1d5059992d8d78
Jinlong Ju <jiju@microsoft.com>
2021-10-14 20:13:06 +0800

    richdata

--- dist/myfunctions.js
+++ dist/myfunctions.js
@@ -737,111 +737,341 @@ function customErrorInput2(inputAllowError, inputAllowErrorOptional, inputAllowE
 
 exports.customErrorInput2 = customErrorInput2;
 
-function customFunctionReturnEntity(entityCase) {
+function SetRichError(errorCase) {
+	switch (errorCase) {
+		case 1: {
+			var error = new CustomFunctions.Error(CustomFunctions.ErrorCode.notAvailable);
+			return error;
+		}
+		case 2: {
+			var error = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue);
+			return error;
+		}
+		case 3: {
+			var error = new CustomFunctions.Error(CustomFunctions.ErrorCode.divisionByZero);
+			return error;
+		}
+		case 4: {
+			var error = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidNumber);
+			return error;
+		}
+		case 5: {
+			var error = new CustomFunctions.Error(CustomFunctions.ErrorCode.nullReference);
+			return error;
+		}
+		case 6: {
+			return {
+				type: "Error",
+				basicValue: "#N/A",
+				basicType: "Error",
+				errorSubType: "HlookupValueNotFound",
+				errorType: "NotAvailable"
+			};
+		}
+		default: {
+			return "Invalid test case";
+		}
+	}
+}
+
+exports.SetRichError = SetRichError;
+
+function SetFormattedNumber(formattedNumberCase) {
+	switch (formattedNumberCase) {
+		case 1: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				1.234,
+				"0.00"
+			);
+			return formattedNumber;
+		}
+		case 2: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				1.234,
+				"$#,##0.00"
+			);
+			return formattedNumber;
+		}
+		case 3: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				1.23,
+				"_($* #,##0.00_);_($* (#,##0.00);_($*??_);_(@_)"
+			);
+			return formattedNumber;
+		}
+		case 4: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				43789,
+				"m/d/yyyy"
+			);
+			return formattedNumber;
+		}
+		case 5: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				45678,
+				"[$-x-systime]h:mm:ss AM/PM"
+			);
+			return formattedNumber;
+		}
+		case 6: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				12345678,
+				"0.00E+00"
+			);
+			return formattedNumber;
+		}
+		case 7: {
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				1.23,
+				"0.00%"
+			);
+			return formattedNumber;
+		}
+		default: {
+			return "Invalid test case";
+		}
+	}
+}
+
+exports.SetFormattedNumber = SetFormattedNumber;
+
+function SetEntity(entityCase) {
 	switch (entityCase) {
 		case 1: {
 			var properties = {
-				"Category": "Communications",
-				"SomeNumber": 1234
+				"TestString": {
+					type: "String",
+					basicValue: "Test"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 1
+				}
 			};
-			var Entity = new CustomFunctions.Entity("[Communications] XYZ Telecom", properties);
+			var Entity = new CustomFunctions.Entity("Basic Entity", properties);
 			return Entity;
 		}
 
 		case 2: {
-			var arrayProperties = {
-				"Children": [
-					["Mary"],
-					["Jane"]
-				],
-				"Zip Code": 9001
+			var properties = {
+				"TestString": {
+					type: "String",
+					basicValue: "Test"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 1
+				},
+				"TestError": new CustomFunctions.Error(CustomFunctions.ErrorCode.divisionByZero)
 			};
-			var Entity = new CustomFunctions.Entity("Bob", arrayProperties);
+			var Entity = new CustomFunctions.Entity("Entity With simple Error", properties);
 			return Entity;
 		}
 
 		case 3: {
+			var properties = {
+				"TestString": {
+					type: "String",
+					basicValue: "Test"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 1
+				},
+				"TestFormattedNumber": new CustomFunctions.FormattedNumber(1.234, "0.00")
+			};
+			var Entity = new CustomFunctions.Entity("Entity With Formatted Number", properties);
+			return Entity;
+		}
+		
+		case 4: {
 			var nestedProperties = {
-				"foo": "bar",
-				"price": 5
+				"TestString2": {
+					type: "String",
+					basicValue: "Test2"
+				},
+				"TestDouble2":{
+					type: "Double",
+					basicValue: 2
+				},
 			};
-
-			var nestedEntity = new CustomFunctions.Entity("Nested Entity", nestedProperties);
-
+			
 			var parentProperties = {
-				"nested": nestedEntity
+				"TestString": {
+					type: "String",
+					basicValue: "Test"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 1
+				},
+				"TestNestedEntity": new CustomFunctions.Entity("Nested Entity", nestedProperties)
 			};
-
-			var parentEntity = new CustomFunctions.Entity("Parent Entity", parentProperties);
+			
+			var parentEntity = new CustomFunctions.Entity("Entity With Nested Object", parentProperties);
 			return parentEntity;
 		}
 
-		case 4: {
-			var properties = {
-				"Info": [
-					["Bold", true],
-					["Underline", false],
-					["Font", 12]
-				],
-				"Sign": true
-			};
-			var Entity = new CustomFunctions.Entity("Document", properties);
-			return Entity;
-		}
-
 		case 5: {
-			var properties = {
-				"SomeNumber": 1234
-			};
-			properties[STRING_LENGTH_256] = "A long string";
-
-			var Entity = new CustomFunctions.Entity("InvalidProperty", properties);
-			return Entity;
+			var EmptyEntity = new CustomFunctions.Entity("Entity Without Properties");
+			return EmptyEntity;
 		}
 
 		case 6: {
-			var properties = {
-				"SomeNumber": 1234
-			};
-
-			var Entity = new CustomFunctions.Entity(STRING_LENGTH_256, properties);
-			return Entity;
+			var EmptyEntity = new CustomFunctions.Entity("Entity Issue", {
+				"TestString":"Test",
+				"TestDouble": 1
+			});
+			return EmptyEntity;
 		}
 
 		case 7: {
-			var Entity = new CustomFunctions.Entity("No property");
-			return Entity;
-		}
+			var EntityWithPorpertyMetadata = new CustomFunctions.Entity("Entity With PropertyMetadata", {
+				TestString: {
+					type: "String",
+					basicValue: "Test",
+					propertyMetadata: {
+						excludeFrom:{
+							"autoComplete":false,
+							"calcCompare":false,
+							"dotNotation":false,
+							"cardView":false
+						},
+						sublabel:"string in entity"
+					}
+				},
+				TestDouble:{
+					type: "Double",
+					basicValue: 1
+				}
+			})
 
+			return EntityWithPorpertyMetadata;
+		}
+		case 8: {
+			var EntityWithPorpertyMetadata = new CustomFunctions.Entity("Entity With PropertyMetadata", {
+				TestString: {
+					type: "String",
+					basicValue: "Test",
+					propertyMetadata: {
+						excludeFrom:{
+							"autoComplete":false,
+							"calcCompare":false,
+							"dotNotation":false,
+							"cardView":true
+						},
+						sublabel:"string in entity"
+					}
+				},
+				TestDouble:{
+					type: "Double",
+					basicValue: 1
+				}
+			})
+
+			return EntityWithPorpertyMetadata;
+		}
+		case 9:{
+			return {"type":"Entity","basicType":"Error","basicValue":"#VALUE!","text":"Entity With API","properties":{"TestDouble":{"type":"Double","basicType":"Double","basicValue":1,"propertyMetadata":{"excludeFrom":{"cardView":true}}},"TestString":{"type":"String","basicType":"String","basicValue":"Test"}}}
+		}
+		case 10:{
+			return {"type":"Entity","basicType":"Error","basicValue":"#VALUE!","text":"Supermetrics Entity",
+			"properties":{
+				"Total spent":{"type":"Double","basicType":"Double","basicValue":1,},
+				"Impressions":{"type":"Double","basicType":"Double","basicValue":304626},
+				"Total spent":{"type":"Double","basicType":"Double","basicValue":4378},
+				"Clicks":{"type":"Double","basicType":"Double","basicValue":880}
+			}}
+	
+		}
 		default: {
-			return "Invalid entityCase"
+			return "Invalid test case";
 		}
 	}
 }
-exports.customFunctionReturnEntity = customFunctionReturnEntity;
 
-function customFunctionReturnEntityArray(entityCase) {
+exports.SetEntity = SetEntity;
+
+function SetWebImage(imageCase) {
+	switch (imageCase) {
+		case 1: {
+			var Image = new CustomFunctions.WebImage("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg");
+			return Image;
+		}
+		case 2: {
+			var Entity = new CustomFunctions.Entity("Animal", {
+				Cat: new CustomFunctions.WebImage("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg")
+			});
+			return Entity;
+		}
+		case 3: {
+			var attribution = new CustomFunctions.Attribution("https://bing.com", "license", "https://bing.com", "source");
+			var image = new CustomFunctions.WebImage("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg", undefined, undefined, [attribution]);
+			return image;
+		}
+		case 4: {
+			var attribution1 = new CustomFunctions.Attribution("https://bing.com", "licenseA", "https://bing.com", "sourceA");
+			var attribution2 = new CustomFunctions.Attribution("https://bing.com", "licenseB", "https://bing.com", "sourceB");
+			var provider = new CustomFunctions.Provider("Powered by Power BI", "https://app.powerbi.com/PowerBILogo.png", "https://msit.powerbi.com/home");
+			var image = new CustomFunctions.WebImage("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg", "Cat Picture", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1920px-Cat_poster_1.jpg", [attribution1, attribution2], provider);
+			return image;
+		}
+		case 5: {
+			var provider = new CustomFunctions.Provider("Powered by Selfhost");
+			var image = new CustomFunctions.WebImage("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg", undefined, undefined, undefined, provider);
+			return image;
+		}
+		case 6: {
+			var attribution = new CustomFunctions.Attribution(undefined, "license");
+			var image = new CustomFunctions.WebImage("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg", undefined, undefined, [attribution]);
+			return image;
+		}
+	}
+}
+
+exports.SetWebImage = SetWebImage;
+
+function SetArray(entityCase) {
 	switch (entityCase) {
 		case 1: {
-			var Entity1 = new CustomFunctions.Entity("First Entity", {
-				"Category": "First",
-				"Priority": 3
+			var entity = new CustomFunctions.Entity("First Entity", {
+				"TestString": {
+					type: "String",
+					basicValue: "Test"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 1
+				}
 			});
-			var Entity2 = new CustomFunctions.Entity("Second Entity", {
-				"Category": "Second",
-				"Priority": 1
-			});
-			return [[Entity1], [Entity2]];
+			var formattedNumber = new CustomFunctions.FormattedNumber(
+				1.234,
+				"0.00"
+			);
+			var error = new CustomFunctions.Error(CustomFunctions.ErrorCode.divisionByZero);
+			return [[entity], [formattedNumber], [error]];
 		}
 
 		case 2: {
 			var Entity1 = new CustomFunctions.Entity("First Entity", {
-				"Category": "First",
-				"Priority": 3
+				"TestString": {
+					type: "String",
+					basicValue: "Test"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 1
+				}
 			});
 			var Entity2 = new CustomFunctions.Entity("Second Entity", {
-				"Category": "Second",
-				"Priority": 1
+				"TestString": {
+					type: "String",
+					basicValue: "Test2"
+				},
+				"TestDouble":{
+					type: "Double",
+					basicValue: 2
+				}
 			});
 			return [
 				["First", Entity1],
@@ -855,43 +1085,59 @@ function customFunctionReturnEntityArray(entityCase) {
 	}
 }
 
-exports.customFunctionReturnEntityArray = customFunctionReturnEntityArray;
+exports.SetArray = SetArray
 
-function customFunctionEntityInput(value, attribute, needStringify) {
-	if (value instanceof CustomFunctions.Entity) {
-		if (attribute == "display")
-			return value.display;
+function getRichData(value, attribute) {
+	// return JSON.stringify(value);
+	if (value.type == CustomFunctions.Entity.valueType) {
+		if (attribute == "text")
+			return value.text;
 		else
 		{
-			if (needStringify)
-			{
-				return JSON.stringify(value.properties[attribute]);
-			}
-			else
-			{
-				return value.properties[attribute];
-			}
+			return value.properties[attribute].basicValue;
 		}
+	}
+	else if(value.type == CustomFunctions.FormattedNumber.valueType) {
+		return value[attribute];
+	}
+	else if(value.type == CustomFunctions.Error.valueType) {
+		return value[attribute];
+	}
+	else if (value.type == CustomFunctions.WebImage.valueType) {
+		if (attribute == "attribution" || attribute == "provider")
+			return JSON.stringify(value[attribute]);
+		else
+			return value[attribute];
 	}
 	else {
 		return "no richData detected";
 	}
 }
 
-exports.customFunctionEntityInput = customFunctionEntityInput;
+exports.getRichData = getRichData
 
-function customFunctionEntityInputArray(value) {
+function getRichDataArray(value) {
 	var ret = [];
 	var oneRow = [];
 	if (value instanceof Array)
 	{
 		for (var i = 0; i < value.length; ++i) {
 			for (var j = 0; j < value[i].length; ++j) {
-				if (value[i][j] instanceof CustomFunctions.Entity) {
-					oneRow.push(value[i][j].display + " detected");
+				var item = value[i][j];
+				if (item.type == CustomFunctions.Entity.valueType) {
+					oneRow.push(item.text);
+				}
+				else if(item.type == CustomFunctions.FormattedNumber.valueType) {
+					oneRow.push(item.basicValue);
+				}
+				else if(item.type == CustomFunctions.Error.valueType) {
+					oneRow.push(item.basicValue);
+				}
+				else if (item.type == CustomFunctions.WebImage.valueType) {
+					oneRow.push(item.address);
 				}
 				else {
-					oneRow.push(value[i][j]);
+					oneRow.push(item);
 				}
 			}
 		}
@@ -901,109 +1147,8 @@ function customFunctionEntityInputArray(value) {
 	return ret;
 }
 
-exports.customFunctionEntityInputArray = customFunctionEntityInputArray;
+exports.getRichDataArray = getRichDataArray;
 
-function formattedNumberReturn(formattedNumberCase) {
-	switch (formattedNumberCase) {
-		case 1: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				1.234,
-				"0.00"
-			);
-			return formattedNumber;
-		}
-		case 2: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				1.234,
-				"$#,##0.00"
-			);
-			return formattedNumber;
-		}
-		break;
-		case 3: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				1.23,
-				"_($* #,##0.00_);_($* (#,##0.00);_($*??_);_(@_)"
-			);
-			return formattedNumber;
-		}
-		break;
-		case 4: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				43789,
-				"m/d/yyyy"
-			);
-			return formattedNumber;
-		}
-		break;
-		case 5: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				45678,
-				"[$-x-systime]h:mm:ss AM/PM"
-			);
-			return formattedNumber;
-		}
-		break;
-		case 6: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				12345678,
-				"0.00E+00"
-			);
-			return formattedNumber;
-		}
-		break;
-		case 7: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				1.23,
-				"0.00%"
-			);
-			return formattedNumber;
-		}
-		break;
-		case 8: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				1.23,
-				"0.00%"
-			);
-			return JSON.stringify(formattedNumber);
-		}
-		break;
-		default: {
-			return "Invalid formattedNumberCase";
-		}
-		break;
-	}
-}
-
-exports.formattedNumberReturn = formattedNumberReturn;
-
-function formattedNumberReturnArray(formattedNumberCase) {
-	switch (formattedNumberCase) {
-		case 1: {
-			var formattedNumber = new CustomFunctions.FormattedNumber(
-				1.234,
-				"0.00"
-			);
-			return [['Hello'],[formattedNumber]];
-		}
-		case 2: {
-			var formattedNumber1 = new CustomFunctions.FormattedNumber(
-				1.234,
-				"0.00"
-			);
-			var formattedNumber2 = new CustomFunctions.FormattedNumber(
-				1.234,
-				"$#,##0.00"
-			);
-			return [[formattedNumber1],[formattedNumber2]];
-		}
-		default: {
-			return "Invalid formattedNumberCase"
-		}
-	}
-}
-
-exports.formattedNumberReturnArray = formattedNumberReturnArray;
 /**
  * Writes a message to console.log().
  * @customfunction LOG
@@ -1031,14 +1176,13 @@ CustomFunctions.associate("GetParameterAddressesFalse", GetParameterAddressesFal
 CustomFunctions.associate("GetParameterAddressesOff", GetParameterAddressesOff);
 CustomFunctions.associate("customErrorInputInvalidArray", customErrorInputInvalidArray);
 CustomFunctions.associate("customErrorInput2", customErrorInput2);
-CustomFunctions.associate("customErrorTest", customErrorTest);
-CustomFunctions.associate("customErrorTest2", customErrorTest2);
-CustomFunctions.associate("customFunctionReturnEntity", customFunctionReturnEntity);
-CustomFunctions.associate("customFunctionReturnEntityArray", customFunctionReturnEntityArray);
-CustomFunctions.associate("customFunctionEntityInput", customFunctionEntityInput);
-CustomFunctions.associate("customFunctionEntityInputArray", customFunctionEntityInputArray);
-CustomFunctions.associate("formattedNumberReturn", formattedNumberReturn);
-CustomFunctions.associate("formattedNumberReturnArray", formattedNumberReturnArray);
+CustomFunctions.associate("SetRichError", SetRichError);
+CustomFunctions.associate("SetFormattedNumber", SetFormattedNumber);
+CustomFunctions.associate("SetEntity", SetEntity);
+CustomFunctions.associate("SetWebImage", SetWebImage);
+CustomFunctions.associate("SetArray", SetArray);
+CustomFunctions.associate("getRichData", getRichData);
+CustomFunctions.associate("getRichDataArray", getRichDataArray);
 /***/ })
 
 /******/ });
